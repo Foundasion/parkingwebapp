89685ddd8017e93bf9838dfc973e8de1c2674061
oiRazor <rozariocrossley1996@gmail.com>
2017-02-25 16:38:33 -0500

    h

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var mapimg;
+
+var clat = 40.23;
+var clon = -74.29
+
+var ww = 1024;
+var hh = 512;
+
+var zoom = 1;
+var earthquakes;
+
+function pre() {
+  mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
+    clat + ',' + clon + ',' + zoom + '/' +
+    ww + 'x' + hh +
+    '?access_token=pk.eyJ1IjoiY29kaW5ndHJhaW4iLCJhIjoiY2l6MGl4bXhsMDRpNzJxcDh0a2NhNDExbCJ9.awIfnl6ngyHoB3Xztkzarw');
+  // earthquakes = loadStrings('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv');
+}
+
+function mercX(lon) {
+  lon = radians(lon);
+  var a = (256 / PI) * pow(2, zoom);
+  var b = lon + PI;
+  return a * b;
+}
+
+function mercY(lat) {
+  lat = radians(lat);
+  var a = (256 / PI) * pow(2, zoom);
+  var b = tan(PI / 4 + lat / 2);
+  var c = PI - log(b);
+  return a * c;
+}
+
+
+function setup() {
+  createCanvas(ww, hh);
+  translate(width / 2, height / 2);
+  imageMode(CENTER);
+  image(mapimg, 0, 0);
+
+  var cx = mercX(clon);
+  var cy = mercY(clat);
+
+}
