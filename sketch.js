11ac7b553179b6b915708fe54490d3804dd1283c
oiRazor <rozariocrossley1996@gmail.com>
2017-02-25 19:36:29 -0500

    edit sketch

--- sketch.js
+++ sketch.js
@@ -1,45 +1,20 @@
 var mapimg;
+var long = -74.1793;
+var lat = 40.7423;
+var zoomLvl = 17;
+var horPixel = 800;
+var vertPixel = 500;
 
-var clat = 40.23;
-var clon = -74.29
-
-var ww = 1024;
-var hh = 512;
-
-var zoom = 1;
-var earthquakes;
-
-function pre() {
-  mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
-    clat + ',' + clon + ',' + zoom + '/' +
-    ww + 'x' + hh +
-    '?access_token=pk.eyJ1IjoiY29kaW5ndHJhaW4iLCJhIjoiY2l6MGl4bXhsMDRpNzJxcDh0a2NhNDExbCJ9.awIfnl6ngyHoB3Xztkzarw');
-  // earthquakes = loadStrings('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv');
+function preload() {
+  // mapimg = loadImage('https://api.mapbox.com/v4/mapbox.streets/' + long + ',' + lat + ',' + zoomLvl + '/' + horPixel + 'x' + vertPixel + '.png?access_token=' + access_token);
+  mapimg = loadImage('https://api.mapbox.com/v4/mapbox.streets/-74.1793,40.7423,17/800x500.png?access_token=pk.eyJ1IjoibmFzdHlkZXYiLCJhIjoiY2l6bHcxMTltMDI0ZjJ3azcxOWZuMzhmYiJ9.XZQzdWZdTb1Xvz9li7E9YA');
 }
-
-function mercX(lon) {
-  lon = radians(lon);
-  var a = (256 / PI) * pow(2, zoom);
-  var b = lon + PI;
-  return a * b;
-}
-
-function mercY(lat) {
-  lat = radians(lat);
-  var a = (256 / PI) * pow(2, zoom);
-  var b = tan(PI / 4 + lat / 2);
-  var c = PI - log(b);
-  return a * c;
-}
-
 
 function setup() {
-  createCanvas(ww, hh);
-  translate(width / 2, height / 2);
-  imageMode(CENTER);
-  image(mapimg, 0, 0);
+  createCanvas(1000,1000);
+  image(mapimg,0,0);
+}
 
-  var cx = mercX(clon);
-  var cy = mercY(clat);
+function draw(){
 
 }
