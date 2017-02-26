9a4f82c34c1d21a45acee8670b73c22eb07e0bef
shahhaard47 <shaard74@gmail.com>
2017-02-26 04:56:48 -0500

    various optimzations

--- sketch.js
+++ sketch.js
@@ -3,35 +3,68 @@ var zoomLvl = 17;
 var centerLat = 40.744052; //njit Lat
 var centerLong = -74.177751; //njit Long
 
-function myMap() {
-  var myCenter = new google.maps.LatLng(centerLat, centerLong); //njit coordinate
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {center: myCenter, zoom: zoomLvl, minZoom: 17, maxZoom: 17, scrollwheel: false,};
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-  var centerMarker = new google.maps.Marker({position: myCenter});
-  //new google.maps.event.trigger( marker, 'click' );
-  centerMarker.setMap(map);
-  //40.743975
-  var myCenter2 = new google.maps.LatLng(40.743975,-74.177392);
-  var marker2 = new google.maps.Marker({position:myCenter2});
-  marker2.setMap(map);
-  var infowindow;
-  centerMarker.addListener('click', function() {
-    infowindow = new google.maps.InfoWindow({
-      content: "10A.M.-6P.M."
-    });
-    infowindow.open(map,centerMarker);
+var lat = [40.744052 ,40.743975];
+var long= [-74.177751 ,-74.177392];
+var time = ["10A.M.-6P.M.", "12P.M.-6P.M."];
 
-  });
-  marker2.addListener('click', function (){
-    infowindow = new google.maps.InfoWindow({
-      content: "12P.M.-6P.M."
+function myMap() {
+  var myCenter = new google.maps.LatLng(lat[0], long[0]); //njit coordinate
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: zoomLvl,};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+
+  // var marker = [2]; //built below
+  // var infowindow = [2]; //built below
+  //
+  // for (var i=0; i < long.length; i++){
+  //   var loc = new google.maps.LatLng(lat[i], long[i]);
+  //   marker[i] = new google.maps.Marker({position: loc});
+  //   marker[i].setMap(map);
+  //   marker[i].addListener('click', function(){
+  //     infowindow[i] = new google.maps.InfoWindow({
+  //       content: time[i]
+  //     });
+  //     infowindow[i].open(map, marker[i]);
+  //     setTimeout(function () { infowindow[i].close(); }, 5000);
+  //   });
+  // }
+
+  var marker = new google.maps.Marker({position: myCenter});
+  marker.setMap(map);
+  marker.addListener('click', function(){
+    var infowindow = new google.maps.InfoWindow({
+      content: "blah"
     });
-    infowindow.open(map,marker2);
-  });
+    infowindow.open(map, marker);
+    setTimeout(function () {infowindow.close(); }, 1000 )
+  })
+
+  // var centerMarker = new google.maps.Marker({position: myCenter});
+  // //new google.maps.event.trigger( marker, 'click' );
+  // centerMarker.setMap(map);
+  // //40.743975
+  // var myCenter2 = new google.maps.LatLng(40.743975,-74.177392);
+  // var marker2 = new google.maps.Marker({position:myCenter2});
+  // marker2.setMap(map);
+  // var infowindow;
+  // centerMarker.addListener('click', function() {
+  //   infowindow = new google.maps.InfoWindow({
+  //     content: "10A.M.-6P.M."
+  //   });
+  //   infowindow.open(map,centerMarker);
+  //
+  // });
+  // marker2.addListener('click', function (){
+  //   infowindow = new google.maps.InfoWindow({
+  //     content: "12P.M.-6P.M."
+  //   });
+  //   infowindow.open(map,marker2);
+  // });
 
   google.maps.event.addListener(map, 'click', function(){
-    infowindow.close();
+    for (var i = 0; i < infowindow.length; i++){
+      infowindow[i].close();
+    }
   });
 }
 
