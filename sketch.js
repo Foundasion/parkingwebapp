13f5e1b8e96e41a5f006df5e24d8385fc0f08a65
oiRazor <rozariocrossley1996@gmail.com>
2017-02-26 00:00:03 -0500

    added vars

--- sketch.js
+++ sketch.js
@@ -2,19 +2,30 @@ var map;
 var zoomLvl = 17;
 var centerLat = 40.744052; //njit Lat
 var centerLong = -74.177751; //njit Long
+var map;
+var myCenter;
+var mapOptions;
+var mapCanvas;
+var centerMarker;
+var pSpaceOne;
+var pSpaceOneM;
+var pSpaces = [centerMarker, pSpaceOneM];
 
 function myMap() {
-  var myCenter = new google.maps.LatLng(centerLat, centerLong); //njit coordinate
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {center: myCenter, zoom: zoomLvl}
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-  var centerMarker = new google.maps.Marker({position: myCenter});
+  myCenter = new google.maps.LatLng(centerLat, centerLong); //njit coordinate
+  mapCanvas = document.getElementById("map");
+  mapOptions = {center: myCenter, zoom: zoomLvl}
+  map = new google.maps.Map(mapCanvas, mapOptions);
+  centerMarker = new google.maps.Marker({position: myCenter});
   //new google.maps.event.trigger( marker, 'click' );
   centerMarker.setMap(map);
     //40.743975
-  var myCenter2 = new google.maps.LatLng(40.743975,-74.177392);
-  var marker2 = new google.maps.Marker({position:myCenter2});
-  marker2.setMap(map);
+  pSpaceOne = new google.maps.LatLng(40.743975,-74.177392);
+  pSpaceOneM = new google.maps.Marker({position: pSpaceOne});
+  pSpaceOneM.setMap(map);
+  for(int i = 0; i<pSpaces.length(); i++){
+    if(pSpaces[i].addListener)
+  }
   centerMarker.addListener('click', function() {
       console.log("HEEEEEEEEEEEEEEEEEEEEEEEE");
       });
