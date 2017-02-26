fee326e54b6015d7cda0b5d3faf66992c9a89eda
oiRazor <rozariocrossley1996@gmail.com>
2017-02-25 23:14:03 -0500

    edit html css js

--- sketch.js
+++ sketch.js
@@ -1,49 +1,18 @@
 var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+
+function myMap() {
+  var myCenter = new google.maps.LatLng(40.744052,-74.177751);//central avenue
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: 17}
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  //new google.maps.event.trigger( marker, 'click' );
+  marker.setMap(map);
+    //40.743975
+  var myCenter2 = new google.maps.LatLng(40.743975,-74.177392);
+  var marker2 = new google.maps.Marker({position:myCenter2});
+  marker2.setMap(map);
+  marker.addListener('click', function() {
+      console.log("HEEEEEEEEEEEEEEEEEEEEEEEE");
+      });
 }
-
-function createButton(context, func){
-    var button = document.createElement("input");
-    button.type = "button";
-    button.value = "im a button";
-    button.onclick = func;
-    context.appendChild(button);
-}
-
-window.onload = function(){
-    createButton(document.body, function(){
-        highlight(this.parentNode.childNodes[1]);
-        // Example of different context, copied function etc
-        // createButton(this.parentNode, this.onclick);
-    });
-}
-/*var mapimg;
-var long = -74.1793;
-var lat = 40.7423;
-var zoomLvl = 17;
-var horPixel = 800;
-var vertPixel = 500;
-
-function preload() {
-  // mapimg = loadImage('https://api.mapbox.com/v4/mapbox.streets/' + long + ',' + lat + ',' + zoomLvl + '/' + horPixel + 'x' + vertPixel + '.png?access_token=' + access_token);
-  mapimg = loadImage('https://api.mapbox.com/v4/mapbox.streets/-74.1793,40.7423,17/800x500.png?access_token=pk.eyJ1IjoibmFzdHlkZXYiLCJhIjoiY2l6bHcxMTltMDI0ZjJ3azcxOWZuMzhmYiJ9.XZQzdWZdTb1Xvz9li7E9YA');
-}
-
-function setup() {
-  createCanvas(1000,1000);
-  image(mapimg,0,0);
-}
-
-function draw(){
-
-<<<<<<< HEAD
-}*/
-=======
-}
-
-//haard's comment
->>>>>>> 680a59b31c31dd08f64e873f1067d63efa3df68d
